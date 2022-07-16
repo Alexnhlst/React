462df3fb0c2b7a12b3b1a0f2f5210dec44093899
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-07-16 21:17:40 +0200

    Updated unit testing section

---
+++ basics/Modash.js
@@ -0,0 +1,27 @@
+const truncate = (string, length) => {
+  if (string.length > length) {
+    return string.slice(0, length) + "...";
+  } else {
+    return string;
+  }
+};
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+const camelCase = (string) => {
+  const words = string.split(/[\s|\-|_]+/);
+  return [
+    words[0].toLowerCase(),
+    ...words.slice(1).map((w) => capitalize(w)),
+  ].join("");
+};
+
+const Modash = {
+  truncate,
+  capitalize,
+  camelCase,
+};
+
+export default Modash;
